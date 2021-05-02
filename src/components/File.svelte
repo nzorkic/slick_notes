87ec86df4c1b7c8304c6f8c6d4b4f1881e77d333
Nikola Zorkić <zorkicdev@gmail.com>
2021-05-02 08:18:53 +0200

    checked selected UI

--- src/components/File.svelte
+++ src/components/File.svelte
@@ -4,31 +4,55 @@
   export let type;
   export let id;
 
-  let selected = $active;
+  window.onload = () => {
+    if ($active) {
+      document.querySelector(`.check-${$active}`).classList.add("show");
+    }
+  };
 
-  $: active.set(selected);
+  const selectActive = (id) => {
+    if (document.querySelector(`.check-${$active}`)) {
+      document.querySelector(`.check-${$active}`).classList.remove("show");
+    }
+    document.querySelector(`.check-${id}`).classList.add("show");
+    active.set(id);
+  };
 </script>
 
-<span style="background-image: url(images/icons/{type}.svg)"
-  >{name}
-  {#if selected === id}
-    <input
-      type="radio"
-      name="active"
-      bind:group={selected}
-      value={id}
-      selected
-    />
-  {:else}
-    <input type="radio" name="active" bind:group={selected} value={id} />
-  {/if}</span
+<div
+  on:click={selectActive(id)}
+  class="tile"
+  style="background-image: url(images/icons/{type}.svg)"
 >
+  {name}
+  <img class="check-{id}" alt="selected" src="/images/icons/checkmark.svg" />
+</div>
 
 <style>
-  span {
+  .tile {
+    display: inline-block;
+    vertical-align: middle;
     padding-left: 1.5em;
     background: left no-repeat;
     font-size: large;
     background-size: 1.4em 1.4em;
+    height: 32px;
+    width: 90%;
+  }
+
+  .tile:hover {
+    cursor: pointer;
+    background-color: rgb(231, 245, 255);
+  }
+
+  img {
+    visibility: hidden;
+    vertical-align: middle;
+    height: 1em;
+    width: 1em;
+  }
+
+  .show {
+    visibility: visible;
   }
 </style>
